bb2d8b829fcaa7a17c334223cbfc10fce6e9b2a1
jacksen <951532416@qq.com>
2017-05-03 09:39:41 +0800

    update

--- day16/__init__.py
+++ day16/__init__.py
@@ -36,3 +36,28 @@ print L
 # print '─'
 # a = '│   └── local.yaml'
 # print a.split('├── ')
+
+
+
+
+print '---------------- 练习题 ---------------------'
+
+
+def proc1():
+    j, k = 3, 4
+    print 'j == %d and k == %d' % (j, k)
+    k = 5
+
+
+def proc2():
+    j = 6
+    proc1()
+    print 'j == %d and k == %d' % (j, k)
+
+k = 7
+proc1()
+# print 'j == %d and k == %d' % (j, k)
+
+j = 8
+proc2()
+print 'j == %d and k == %d' % (j, k)
