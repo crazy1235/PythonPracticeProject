bd173dfe57e3d3ad329e645ed852534b80823092
jacksen <yansen19920101@gmail.com>
2017-05-15 09:58:33 +0800

    update

--- test/__init__.py
+++ test/__init__.py
@@ -15,3 +15,14 @@ names2[0] = 'jack'
 print names1
 print names2
 print names3
+
+print '---------------------------'
+
+confusion = {}
+confusion[1] = 1
+confusion[1.0] = 2
+confusion[1.1] = 3
+confusion['1'] = 4
+
+print confusion
+
